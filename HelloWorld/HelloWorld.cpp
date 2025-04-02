f3aa8b5246527d71e1c97ceb136d039f833861fc
CodyAndGity <mdavis@student.neumont.edu>
2025-04-02 14:45:16 -0600

    Demo class code

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,7 +1,15 @@
+#include "Student.h"
 #include <iostream>
 using namespace std;
 
+
+
 int main() {
+    Student student;
+    student.SetName("Bob");
+
+    student.Write();
+    f();
     cout << "Hello, World!" << endl;
     cout << "---GitHub Online---" << endl;
     return 0;
