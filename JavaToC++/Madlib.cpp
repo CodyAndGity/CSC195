70be64f7d60ddd772e4c5136a8c1533bda8cbfed
CodyAndGity <mdavis@student.neumont.edu>
2025-04-04 22:08:43 -0600

    Changed story slightly

--- JavaToC++/Madlib.cpp
+++ JavaToC++/Madlib.cpp
@@ -11,7 +11,7 @@ string Madlib::assembleStory() {
 	int randomNumber = rand() % 3 + 2; // Random number from 2 to 4
 	int amountNumber = stoi(amount) * randomNumber;
 	amount = to_string(amountNumber);
-		string story = "Hello there! That was a crazy " + verb + " you did there! Why, you must of done it "
+		string story = "Hello! That was a crazy " + verb + " you did there! Why, you must of done it "
 		+ amount + " times in row without stopping! It was a " + adjective + " performance!";
 	return story;
 };
